5835bf0d1bb5dc928faca10fe8d5e3a1380070c0
osmancandulger <osmancandulger@gmail.com>
2021-03-08 01:40:22 +0300

    Added sidebar and content-container and styled

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const listItem = document.querySelectorAll("li");
+listItem.forEach((e) => {
+  e.addEventListener("click", () => {
+    const active = document.querySelector(".active");
+
+    if (active) {
+      active.classList.remove("active");
+    }
+    e.classList.toggle("active");
+  });
+});
